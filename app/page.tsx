afe37ad90127a1d14e7afa4561c1277b355b04d1
eunwoo-levi <eunwoo1341@gmail.com>
2024-07-16 21:36:09 +0900

    updating

--- app/page.tsx
+++ app/page.tsx
@@ -36,43 +36,66 @@ export default function Home() {
         </span>
       </div>
 
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <div className="flex gap-4">
-          <button
-            onClick={handleCreateChat}
-            className="bg-green-100 p-1 rounded-md font-medium hover:scale-110 transition active:bg-purple-500"
-          >
-            Create Chat Room
-          </button>
-          {accessToken ? (
-            <button
-              onClick={handleLogout}
-              className="bg-white p-1 rounded-md font-semibold"
-            >
-              Logout
-            </button>
-          ) : (
-            <Link
-              className="bg-white p-1 rounded-md font-semibold"
-              href="/login"
-            >
-              Login
-            </Link>
-          )}
+      <div className="bg-bgPic bg-cover bg-center min-h-screen flex flex-col justify-center items-center gap-8 relative">
+        <div>
+          <span className="text-white text-4xl font-bold">
+            IN PURSUIT OF JUSTICE, EVERYDAY
+          </span>
         </div>
-        {showModal && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="bg-white p-6 rounded-md shadow-md w-full max-w-md relative">
+        <div className="bg-white bg-opacity-50 w-full max-w-[90%] mx-auto p-4 rounded-lg shadow-lg">
+          <div className="max-w-md mx-auto flex flex-col items-center space-y-4">
+            <div className="bg-blue-300 p-3 rounded-lg flex flex-col w-full">
+              <span className="text-white font-semibold text-center">
+                새로운 채팅을 만들어 보세요!
+              </span>
               <button
-                onClick={closeModal}
-                className="absolute top-2 right-2 bg-gray-200 p-2 rounded-full"
+                onClick={handleCreateChat}
+                className="bg-green-100 p-1 rounded-md font-medium hover:scale-110 transition active:bg-purple-500"
               >
-                X
+                Create Chat Room
               </button>
-              <CreateChatRoomForm onClose={closeModal} />
             </div>
+
+            {accessToken ? (
+              <button
+                onClick={handleLogout}
+                className="bg-white p-1 rounded-md font-semibold"
+              >
+                Logout
+              </button>
+            ) : (
+              <div>
+                <span className="text-slate-500 mx-4">로그인 하기</span>
+                <Link
+                  className="bg-white p-1 rounded-md font-semibold"
+                  href="/login"
+                >
+                  Login
+                </Link>
+              </div>
+            )}
+            {showModal && (
+              <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+                <div className="bg-white p-6 rounded-md shadow-md w-full max-w-md relative">
+                  <button
+                    onClick={closeModal}
+                    className="absolute top-2 right-2 bg-gray-200 p-2 rounded-full"
+                  >
+                    X
+                  </button>
+                  <CreateChatRoomForm onClose={closeModal} />
+                </div>
+              </div>
+            )}
           </div>
-        )}
+        </div>
+        <div className="absolute bottom-10 bg-gray-500 bg-opacity-40 w-full h-10 max-w-[40%] flex justify-center items-center rounded-md">
+          <div className="">
+            <button className="bg-amber-900 rounded-md p-1 text-white hover:scale-110">
+              Contact
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
