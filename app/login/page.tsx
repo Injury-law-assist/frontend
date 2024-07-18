48aa5bd15b1f43f5d7c2b6e745671bcf675c4d73
KNU-K <knukang334@gmail.com>
2024-07-18 23:31:26 +0900

    fix : 로그인 & 회원가입 UI 개선

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,11 @@
 import AccountForm from "@/components/page/AccountForm";
 
-export default function page() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md space-y-8">
-        <div className="flex justify-center pb-6">
-          <h1 className="text-3xl text-gray-900 font-extrabold">
-            Sign in to your account
-          </h1>
+export default function Page() {
+    return (
+        <div className="bg-gradient-to-b from-blue-100 to-white dark:from-gray-900 dark:to-black min-h-screen bg-gray-100 flex flex-col justify-center items-center px-4 sm:px-4 lg:px-20 py-8">
+            <div className="max-w-screen-md w-full space-y-4">
+                <AccountForm />
+            </div>
         </div>
-        <AccountForm />
-      </div>
-    </div>
-  );
+    );
 }
